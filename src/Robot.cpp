4a4d8faef1919162c67f85ec026ea28f3e7963df
Timothy Steward <tsteward0@gmail.com>
2015-01-13 10:48:16 -0600

    Rearrange Files, added gitignore

---
+++ src/Robot.cpp
@@ -0,0 +1,43 @@
+#include "WPILib.h"
+
+/**
+ * This is a demo program showing the use of the RobotDrive class.
+ * The SampleRobot class is the base of a robot application that will automatically call your
+ * Autonomous and OperatorControl methods at the right time as controlled by the switches on
+ * the driver station or the field controls.
+ *
+ * WARNING: While it may look like a good choice to use for your code if you're inexperienced,
+ * don't. Unless you know what you are doing, complex code will be much more difficult under
+ * this system. Use IterativeRobot or Command-Based instead if you're new.
+ */
+class Robot: public SampleRobot
+{
+
+public:
+	Robot()
+	{
+	}
+
+	/**
+	 * Drive left & right motors for 2 seconds then stop
+	 */
+	void Autonomous()
+	{
+	}
+
+	/**
+	 * Runs the motors with arcade steering.
+	 */
+	void OperatorControl()
+	{
+	}
+
+	/**
+	 * Runs during test mode
+	 */
+	void Test()
+	{
+	}
+};
+
+START_ROBOT_CLASS(Robot);
